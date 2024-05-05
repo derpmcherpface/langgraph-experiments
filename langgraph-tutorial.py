4648dbbb414625b9fc727c30500162ca0b5a9f4d
derpmcherpface <kalle.anka@ankeborg.an>
2024-05-05 17:10:27 +0200

    working model invocation

--- langgraph-tutorial.py
+++ langgraph-tutorial.py
@@ -10,12 +10,12 @@ from langchain_core.runnables import RunnableLambda
 function_1_runnable = RunnableLambda(function_1)
 
 def function_2(input_2):
-    #result=input_2
-    #return result #passthrough, invoke model here
-    #response = model.invoke(input_2)
-    print("invoking model with question:")
-
+    print("invoking model with question: " + input_2)
+    return input_2
 function2_runnable=RunnableLambda(function_2)
+
+from langchain_community.chat_models import ChatOllama
+model = ChatOllama(model="openhermes")
 
 from langgraph.graph import Graph
 
@@ -26,18 +26,20 @@ workflow = Graph()
 print("adding nodes")
 workflow.add_node("node_1", function_1_runnable)
 workflow.add_node("node_2", function2_runnable)
+workflow.add_node("node_3", model) # models are runnables, i.e. valid nodes
 
 print("adding edges")
 workflow.add_edge('node_1', 'node_2')
+workflow.add_edge('node_2', 'node_3')
 
 workflow.set_entry_point("node_1")
-workflow.set_finish_point("node_2")
+workflow.set_finish_point("node_3")
 
 print("compiling workflow")
 app = workflow.compile()
 
 print("invoking app")
-result=app.invoke("Hello")
+result=app.invoke("") # consider removing input to function_1
 print(result); 
 
 # Adding LLM Call 
