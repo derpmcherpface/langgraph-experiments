a98e4eeb215114710e070d46e83ddf587e8e3d73
derpmcherpface <kalle.anka@ankeborg.an>
2024-05-10 17:39:11 +0200

    cleanup

--- langgraph-tutorial.py
+++ langgraph-tutorial.py
@@ -6,26 +6,19 @@ model = ChatOllama(model="openhermes")
 def function_1(state):
     #result=input_1 + " Hi "
     print("What is your question?")
-    #result=input()
-    result="Who was Napoleon?"
+    result=input()
+    #result="Who was Napoleon?"
     state['messages'].append(result)
     return state
 from langchain_core.runnables import RunnableLambda
 function_1_runnable = RunnableLambda(function_1)
 
-# State consistency check
-def function_2(state):
+# model invocation node function
+def function_3(state):
     print("state: " + str(state))
     messages=state['messages']
     input_2=messages[-1]
-    print("invoking model with question: " + input_2)
-    return state
-function2_runnable=RunnableLambda(function_2)
-
-# model invocation node function
-def function_3(state):
-    messages=state['messages']
-    input_2=messages[-1]
+    print("input_2:" + input_2)
     #response = model.invoke("Who was Napoleon?")
     response = model.invoke(input_2)
     print("response: " + str(response))
@@ -42,10 +35,6 @@ AgentState = {}
 # We will append new messages as we pass along nodes.
 AgentState["messages"] = []
 
-
-
-
-
 from langgraph.graph import Graph
 
 # Define a Langchain graph
@@ -54,19 +43,12 @@ workflow = Graph()
 
 print("adding nodes")
 workflow.add_node("node_1", function_1_runnable)
-workflow.add_node("node_2", function2_runnable)
-#workflow.add_node("model_node", model) # models are runnables, i.e. valid nodes
 
 workflow.add_node("node_3", function_3_runnable)
 
 print("adding edges")
-workflow.add_edge('node_1', 'node_2')
-#workflow.add_edge('node_2', 'model_node')
-#workflow.add_edge('model_node', 'node_3')
-workflow.add_edge('node_2', 'node_3')
-#workflow.add_edge('node_3', 'node_1')
+workflow.add_edge('node_1', 'node_3')
 workflow.add_conditional_edges('node_3', router)
-
 workflow.set_entry_point("node_1")
 #workflow.set_finish_point("node_3")
 
@@ -74,6 +56,5 @@ print("compiling workflow")
 app = workflow.compile()
 
 print("invoking app")
-#result=app.invoke("") # consider removing input to function_1
 result=app.invoke(AgentState)
 #print(result); 
