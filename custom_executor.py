755ca8fb17e86236ea939a7550942ab685433ce9
derpmcherpface <kalle.anka@ankeborg.an>
2024-05-25 11:22:12 +0200

    add model invocation node

--- custom_executor.py
+++ custom_executor.py
@@ -2,6 +2,7 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.runnables import RunnableLambda
 from langgraph.graph import Graph
 import queue
+from functools import partial
 class CustomExecutor: 
 
     # static methods used as nodes in the graph
@@ -13,13 +14,18 @@ class CustomExecutor:
         state['question']=result
         return state
     
+    def invocation_node_fcn(model,state):
+        print("running node invocation fcn")
+        return state
+    
     def invoke_executor(self): 
-        return self.model.invoke("Hi! how are you?")
+        return self.app.invoke(self.AgentState)
 
     def __init__(self):
         print("Starting initialize custom executor")
         self.model = ChatOllama(model="openhermes")
         self.input_node_runnable = RunnableLambda(CustomExecutor.input_node_fcn)
+        self.invocation_node_runnable = RunnableLambda(partial(CustomExecutor.invocation_node_fcn, self.model))
 
         # messages key will be assigned as an empty array. 
         # We will append new messages as we pass along nodes.
@@ -31,8 +37,10 @@ class CustomExecutor:
 
         self.workflow = Graph()
         self.workflow.add_node("input_node", self.input_node_runnable)
+        self.workflow.add_node("invocation_node", self.invocation_node_runnable)
+        self.workflow.add_edge('input_node', 'invocation_node')
         self.workflow.set_entry_point("input_node")
-        self.workflow.set_finish_point("input_node")
+        self.workflow.set_finish_point("invocation_node")
         self.app = self.workflow.compile()
 
         print("Custom executor initialized")
