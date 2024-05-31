abfd967c2565262844443c3f035653191ca1582b
derpmcherpface <kalle.anka@ankeborg.an>
2024-05-31 19:12:07 +0200

    add test_memory_custom_executor

--- langgraphtest.py
+++ langgraphtest.py
@@ -17,5 +17,19 @@ class TestSumTest(unittest.TestCase):
         self.customExecutor.add_input("My carpet is green. What color is my carpet?")
         print("response: " + str(self.customExecutor.invoke_executor()))
 
+
+    def test_memory_custom_executor(self):
+        self.customExecutor = CustomExecutor()
+        self.customExecutor.add_input("My carpet is green.")
+        self.response1=self.customExecutor.invoke_executor()
+        print("response: " + str(self.response1))
+        self.customExecutor.add_input("What color is my carpet?")
+        self.response2=self.customExecutor.invoke_executor()
+        print("response: " + str(self.response2))
+        if str(self.response2).lower().find("green") != -1:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
+
 if __name__ == '__main__':
     unittest.main()
