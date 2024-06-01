4204541c49bcc1b7f51d1bdabaace45e43dad35a
derpmcherpface <kalle.anka@ankeborg.an>
2024-06-01 08:09:01 +0200

    add reasoning test

--- langgraphtest.py
+++ langgraphtest.py
@@ -4,18 +4,21 @@ import timeout_decorator
 
 class TestSumTest(unittest.TestCase):
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 3)), 6, "Should be 6")
-
-
-    #@timeout_decorator.timeout(5)
     def test_init_custom_executor(self):
         self.customExecutor = CustomExecutor()
         self.customExecutor.add_input("My carpet is green. What color is my carpet?")
         print("response: " + str(self.customExecutor.invoke_executor()))
+
+    #@timeout_decorator.timeout(5)
+    def test_reasoning_custom_executor(self):
+        self.customExecutor = CustomExecutor()
+        self.customExecutor.add_input("My carpet is green. What color is my carpet?")
+        self.response2=self.customExecutor.invoke_executor()
+        print("response: " + str(self.response2))
+        if str(self.response2).lower().find("green") != -1:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
 
 
     def test_memory_custom_executor(self):
